35079e94251dced774b397f5c06bf808a471d95f
ManojSandy <MoojeSandy@gmail.com>
2018-10-25 15:04:38 +0530

    Commit3

---
+++ TaskManager.NUnit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TaskManager.NUnit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TaskManager.NUnit")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("584ac986-44de-4d48-a801-a20c0700927f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TaskManager.NUnit/TaskManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskManager.WebAPI.Controllers;
+using TaskManager.BL;
+using TaskManager.Entities;
+using System.Collections.Generic;
+
+namespace TaskManager.NUnit
+{
+    [TestClass]
+    public class TaskManager
+    {
+        [TestMethod]
+        public void GetAllTasks()
+        {
+            var testTasks = GetTestTasks();
+            var controller = new TaskManagerController();
+
+            var result = controller.GetAllTasks() as List<TaskTable>;
+            Assert.AreEqual(testTasks.Count, result.Count);
+        }
+
+        private List<TaskTable> GetTestTasks()
+        {
+            var testProducts = new List<TaskTable>();
+            testProducts.Add(new TaskTable { TaskId = 1, ParentTask = "", TaskName = "Sandy", StartDate = Convert.ToDateTime("2018-10-18"), EndDate = Convert.ToDateTime("2018-10-18"), Priority = 20, Deleted = false});
+           
+            return testProducts;
+        }
+    }
+}
